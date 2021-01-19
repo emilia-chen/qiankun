e57f29efc06fa4e04afe306fc9f36e93ec66e0f8
陈嘉诚 <jiacheng.chen@hand-china.com>
2021-01-19 21:57:10 +0800

    a

--- mainApp/src/App.vue
+++ mainApp/src/App.vue
@@ -1,11 +1,12 @@
-<template>
+    
   <div id="main-root">
     <!-- loading -->
     <div v-if="loading">loading</div>
     <!-- 子应用盒子 -->
     <div id="root-view" class="app-view-box" v-html="content"></div>
+
   </div>
-</template>
+
 
 <script>
 export default {
--- mainApp/src/views/main.vue
+++ mainApp/src/views/main.vue
@@ -1,5 +1,33 @@
 <template>
     <div>
         i am 主基站 
+       <div @click='clickApp'>路由加载微应用app1</div>
+        <div @click='loadApp'>加载微应用app1</div>
+        <div ref='showContent'>显示内容</div>
     </div>
 </template>
+<script>
+import { loadMicroApp } from 'qiankun';
+export default {
+  name: "App",
+  mounted () { 
+   console.log('this',this.$refs.showContent)  
+  },
+  methods: {   // 返回s
+    loadApp () {
+      console.log('loadApp')
+       loadMicroApp({     name: "app1",
+            entry: "//localhost:8091",
+            container:document.getElementById('main-root'),//this.$refs.showContent,
+            //
+          props: { name: 'qiankun' },
+       });
+    },
+    clickApp() {
+      this.$router.push({ path:'/app1'
+      })
+    }
+  },
+
+}
+</script>
